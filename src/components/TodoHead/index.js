a94924664a68961800de0f448e4576d7cf1766a1
kjw7953 <uis7953@naver.com>
2022-07-24 04:12:51 +0900

    [feat] 로그아웃 스켈레톤 코드 작성

--- src/components/TodoHead/index.js
+++ src/components/TodoHead/index.js
@@ -10,11 +10,20 @@ function TodoHead({ todos }) {
   });
   const dayName = today.toLocaleDateString("ko-KR", { weekday: "long" });
 
+  const onClickSignOut = () => {
+    // TODO: 로그아웃 로직 작성
+    window.sessionStorage.removeItem('isAuthenticated');
+    window.location.reload();
+  }
+
   return (
     <div className="head-container">
       <h1>{dateString}</h1>
       <div className="day">{dayName}</div>
-      <div className="todos-left">할 일 {undoneTodos.length}개 남음</div>
+      <div className="info-bar">
+        <div className="todos-left">할 일 {undoneTodos.length}개 남음</div>
+        <div onClick={onClickSignOut}>로그아웃</div>
+      </div>
     </div>
   );
 }
