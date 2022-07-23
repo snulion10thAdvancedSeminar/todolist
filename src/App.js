b202b5723556a2dd4820c2f689b9b3433f3e72ea
Juyoung <godante27@gmail.com>
2022-07-24 01:07:46 +0900

    TodoTemplate 분리 - close #11

--- src/App.js
+++ src/App.js
@@ -1,38 +1,8 @@
-import { useState } from 'react';
 import './App.scss';
 import TodoTemplate from './components/TodoTemplate';
-import TodoHead from './components/TodoHead';
-import TodoList from './components/TodoList';
-import TodoCreate from './components/TodoCreate';
 
 function App() {
-  const [nextId, setNextId] = useState(2);
-  const [todos, setTodos] = useState([
-    { id: 1, text: '세미나 잘 듣기', done: false },
-  ]);
-  const onToggle = (id) => {
-    const newTodos = todos.map((todo) =>
-      todo.id === id ? { ...todo, done: !todo.done } : todo
-    );
-    setTodos(newTodos);
-  };
-  const onRemove = (id) => {
-    const newTodos = todos.filter((todo) => todo.id !== id);
-    setTodos(newTodos);
-  };
-  const onCreate = (text) => {
-    const newTodos = [...todos, { id: nextId, text, done: false }];
-    setTodos(newTodos);
-    setNextId(nextId + 1);
-  };
-
-  return (
-    <TodoTemplate>
-      <TodoHead todos={todos} />
-      <TodoList todos={todos} onToggle={onToggle} onRemove={onRemove} />
-      <TodoCreate onCreate={onCreate} />
-    </TodoTemplate>
-  );
+  return <TodoTemplate />;
 }
 
 export default App;
